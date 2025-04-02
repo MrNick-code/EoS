3270df3c2f5ea410464296441858e76f7b782c35
Matheus Capelin <62571818+MrNick-code@users.noreply.github.com>
2025-04-02 01:32:19 -0300

    cvae_to_flow_tt validation working

--- CVAE/cvae_to_flow_tt.py
+++ CVAE/cvae_to_flow_tt.py
@@ -5,49 +5,62 @@ from torch.nn import functional as F
 import data_man_pick_copy as dmp
 import cv2
 import numpy as np
+from numpy import concatenate as concat
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
-
 from torch.amp import autocast, GradScaler
-
 
 # cuda setup
 device = torch.device("cuda")
 torch.cuda.empty_cache()
 # print(f'{torch.cuda.memory_summary(device=None, abbreviated=False)}') # to check cuda memory usage
 
-# hyper params
-out_features, features_multiplier = 64, 1
+# data paths
+EOSXimgs_40 = ["C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSL_low_40b", "C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSQ_low_40"] # 50x50
+EOSXimgs_50 = ["C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSL_low_50", "C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSQ_low_50"] # 60x60
 
-EOSXimgs_40 = ["C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSL_low_40b", "C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSQ_low_40"] # Path (50x50 imgs)
-EOSXimgs_50 = ["C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSL_low_50", "C:\\Users\\mathe\\faculdade\\ML\\EoS\\IMG_DATA\\EOSQ_low_50"] # Path (60x60 imgs)
-
-common_size, batch_size, num_epochs, learning_rate, normalization_term_X, normalization_term_Y = (100, 100), 32, 300, 5e-5, 255, 1e-3
-# 100, 100; 32; 250; 5e-5; 255; 1e-3
+# hyperparameters
+common_size, batch_size, normalization_term_X, normalization_term_Y = (100, 100), 32, 1e-3, 255 # data set
+out_features, features_multiplier = 64, 1                                                       # network
+num_epochs, learning_rate = 300, 5e-5                                                           # fit
+# (100, 100); 32; 255; 1e-3 / 64; (either 1 or 10) / 300; 5e-5 (last best run)
 
 class cvae_load:
     
     def __init__(self, ntX, ntY):
+        '''
+        desc
+        - transform data into pytorch necessities, load, normalize and resize it
+        args
+        - ntX ~ normalization term for |v_n|² (x)
+          ntY ~ normalization term for pho(P_t, phi) (y)
+        returns
+        - data_loader ~ the loader of every dataset (train, validatino and test)
+        '''
         self.ntX = ntX
         self.ntY = ntY
         self.ld = self.loadDataNorm
         self.norm = self.normalize
         self.ri = self.resize_images
 
-    def loadDataNorm(self, EOSXimgs_40, EOSXimgs_50):
+    # load and normalize the data
+    def loadDataNorm(self, EOSXimgs_40, EOSXimgs_50): # X and Y are swapped here in var names (ntx/y is right)
         X_train40, X_val40, X_test40, Y_train40, Y_val40, Y_test40 = dmp.get_data(paths=EOSXimgs_40, shape=(50, 50, 4))
-        X_train40, X_val40, X_test40 = self.norm(X_train40, X_val40, X_test40, self.ntX)
-        Y_train40, Y_val40, Y_test40 = self.norm(Y_train40, Y_val40, Y_test40, self.ntY) # test because Adry norm report
+        # print("Unique values in X_train40 (y) before normalization:", np.unique(X_train40)) # images bin values are okay 
+        X_train40, X_val40, X_test40 = self.norm(X_train40, X_val40, X_test40, self.ntY)
+        Y_train40, Y_val40, Y_test40 = self.norm(Y_train40, Y_val40, Y_test40, self.ntX) # test because Adry norm report
         xy40 = [X_train40, X_val40, X_test40, Y_train40, Y_val40, Y_test40]
         
         X_train50, X_val50, X_test50, Y_train50, Y_val50, Y_test50 = dmp.get_data(paths=EOSXimgs_50, shape=(60, 60, 4))
-        X_train50, X_val50, X_test50 = self.norm(X_train50, X_val50, X_test50, self.ntX)
-        Y_train50, Y_val50, Y_test50 = self.norm(Y_train50, Y_val50, Y_test50, self.ntY)
+        X_train50, X_val50, X_test50 = self.norm(X_train50, X_val50, X_test50, self.ntY)
+        Y_train50, Y_val50, Y_test50 = self.norm(Y_train50, Y_val50, Y_test50, self.ntX)
         xy50 = [X_train50, X_val50, X_test50, Y_train50, Y_val50, Y_test50]
+        # print(X_train40.dtype) # X (y) is float64
+        # print("Unique values in X_train40 (y) after normalization:", np.unique(X_train40))
         return xy40, xy50
     
-    # Normalization function
+    # normalization function
     def normalize(self, train, val, test, normalization_term):
         return train / normalization_term, val / normalization_term, test / normalization_term
 
@@ -55,46 +68,35 @@ class cvae_load:
     def resize_images(self, images, size):
         resized_images = []
         for img in images:
-            resized_img = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
+            resized_img = cv2.resize(img, size, interpolation=cv2.INTER_AREA) # correclty number of color channels
             resized_images.append(resized_img)
         return np.array(resized_images)
 
-    # [Num samples, Hidht, Weight, Chanells] --> [Num samples, Chanells, Hidht, Weight]
-    def NCHW(self, trainX, valX, testX, trainY, valY, testY, trainX2, valX2, testX2, trainY2, valY2, testY2, cs, bs):
+    # transform [Num samples, Hidht, Weight, Chanells] into [Num samples, Chanells, Hidht, Weight]
+    def NCHW(self, trainY, valY, testY, trainX, valX, testX, trainY2, valY2, testY2, trainX2, valX2, testX2, cs, bs):
 
-        trainX = self.ri(trainX, cs)
-        valX = self.ri(valX, cs)
-        testX = self.ri(testX, cs)
+        trainY, valY, testY = self.ri(trainY, cs), self.ri(valY, cs), self.ri(testY, cs) # resize_image(y = pho(P_t, phi))
 
-        trainX2 = self.ri(trainX2, cs)
-        valX2 = self.ri(valX2, cs)
-        testX2 = self.ri(testX2, cs)
+        trainY2, valY2, testY2 = self.ri(trainY2, cs), self.ri(valY2, cs), self.ri(testY2, cs)
 
-        trainX = np.concatenate((trainX, trainX2), axis=0)
-        valX = np.concatenate((valX, valX2), axis=0)
-        testX = np.concatenate((testX, testX2), axis=0)
-        trainY = np.concatenate((trainY, trainY2), axis=0)
-        valY = np.concatenate((valY, valY2), axis=0)
-        testY = np.concatenate((testY, testY2), axis=0)
+        trainY, valY, testY = concat((trainY, trainY2), axis=0), concat((valY, valY2), axis=0), concat((testY, testY2), axis=0)
 
-        b = [torch.from_numpy(j).float() for j in [trainY, valY, testY]]
-        a = [torch.from_numpy(i).float().permute(0, 3, 1, 2) for i in [trainX, valX, testX]]
+        trainX, valX, testX = concat((trainX, trainX2), axis=0), concat((valX, valX2), axis=0), concat((testX, testX2), axis=0)
 
-        train_dataset = TensorDataset(a[0], b[0])
-        val_dataset = TensorDataset(a[1], b[1])
-        test_dataset = TensorDataset(a[2], b[2])
+        b = [torch.from_numpy(j).float() for j in [trainX, valX, testX]]
+        a = [torch.from_numpy(i).float().permute(0, 3, 1, 2) for i in [trainY, valY, testY]]
 
+        train_dataset, val_dataset, test_dataset = TensorDataset(a[0], b[0]), TensorDataset(a[1], b[1]), TensorDataset(a[2], b[2])
 
-        train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True, drop_last=True, pin_memory=True)
-        val_loader = DataLoader(val_dataset, batch_size=bs, shuffle=False, drop_last=True, pin_memory=True)
-        test_loader = DataLoader(test_dataset, batch_size=bs, shuffle=False, drop_last=True, pin_memory=True)
+        train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True, drop_last=True, pin_memory=True) # shuffle    ~ avoid local min 
+        val_loader = DataLoader(val_dataset, batch_size=bs, shuffle=True, drop_last=True, pin_memory=True)     # drop_last  ~ avoid last batch 
+        test_loader = DataLoader(test_dataset, batch_size=bs, shuffle=False, drop_last=True, pin_memory=True)  # pin_memory ~ CUDA related
 
         return train_loader, val_loader, test_loader
 
+# create data loaders
 data_loader = cvae_load(ntX=normalization_term_X, ntY=normalization_term_Y)
-
 xy40, xy50 = data_loader.loadDataNorm(EOSXimgs_40, EOSXimgs_50)
-
 train_loader, val_loader, test_loader = data_loader.NCHW(
                 xy40[0], xy40[1], xy40[2], xy40[3], xy40[4], xy40[5],
                 xy50[0], xy50[1], xy50[2], xy50[3], xy50[4], xy50[5],  
@@ -102,12 +104,17 @@ train_loader, val_loader, test_loader = data_loader.NCHW(
 
 class ConditionalVariationalAutoEncoder(nn.Module):
     def __init__(self, x, y, out_features):
+        '''
+        desc
+        - my CVAE implementation
+        - "LAVA: Latent-Aided Variational Autoregressor" (lmao)
+        '''
         super().__init__()
         self.x = x
         self.y = y
         self.out_features = out_features
 
-        # Encoder convolucional
+        # condition convolutional-encoder 
         self.encoder2d = nn.Sequential(
             nn.Conv2d(in_channels=4, out_channels=16, kernel_size=(8, 8), padding=3),
             nn.Dropout(.2),
@@ -119,20 +126,20 @@ class ConditionalVariationalAutoEncoder(nn.Module):
             nn.ReLU()
         )
 
-        # Descobrir dinamicamente as dimensões de entrada para encoderLin
+        # linear-encoder dynamic inputs 
         with torch.no_grad():
             dummy_x = torch.randn(1, *x.shape[1:]).to(device)
             dummy_y = torch.randn(1, *y.shape[1:]).to(device)
             dummy_y = dummy_y.to(next(self.encoder2d.parameters()).device)
             conv_out = self.encoder2d(dummy_y)
-            flattened_size = conv_out.view(conv_out.size(0), -1).shape[1]
-            flattened_x = dummy_x.view(dummy_x.size(0), -1).shape[1]
-            in_features = flattened_size + flattened_x  # Dimensão correta # here we want the dimension
+            flattened_size = conv_out.view(conv_out.size(0), -1).shape[1]       # view() reshapes a tensor by 'stretching' or
+            flattened_x = dummy_x.view(dummy_x.size(0), -1).shape[1]            # 'squeezing' its elements into the shape you specify.
+            in_features = flattened_size + flattened_x                        # correct dimension
         
         print(f'{flattened_size}, {flattened_x}')
         print(in_features)
 
-        # Definir camada Linear usando o tamanho inferido
+        # linear-encoder q
         self.encoderLin = nn.Sequential(
             nn.Linear(in_features=in_features, out_features=out_features*features_multiplier),
             nn.BatchNorm1d(out_features*features_multiplier),
@@ -152,6 +159,7 @@ class ConditionalVariationalAutoEncoder(nn.Module):
             nn.LeakyReLU()
         )
         
+        # linear-encoder r1 
         self.encoderLinAlt = nn.Sequential(
             nn.Linear(in_features=in_features-3, out_features=out_features*features_multiplier),
             nn.BatchNorm1d(out_features*features_multiplier),
@@ -171,8 +179,10 @@ class ConditionalVariationalAutoEncoder(nn.Module):
             nn.LeakyReLU(negative_slope=0.01)
         )
 
+        # final encoder layer
         self.layer10 = nn.Linear(in_features=16*features_multiplier, out_features=3)
 
+        # linear-decoder 
         self.decoderLin = nn.Sequential(
             nn.Linear(in_features=in_features, out_features=16*features_multiplier),
             nn.BatchNorm1d(16*features_multiplier),
@@ -192,58 +202,85 @@ class ConditionalVariationalAutoEncoder(nn.Module):
             nn.LeakyReLU(negative_slope=0.01),
         )
 
-        self.layer10dec =  nn.Linear(in_features=out_features*features_multiplier, out_features=3) # Has to be 3, so loss function can compare x_targets and x_samp!
+        # final decoder-layer (out_features has to be 3, so loss function can compare x_targets and x_samp)
+        self.layer10dec =  nn.Linear(in_features=out_features*features_multiplier, out_features=3)
 
     def encode(self):
-        conv_out = self.encoder2d(self.y).to(device)      
-        flattened_size = conv_out.view(conv_out.size(0), -1)
-        flattened_x = self.x.view(self.x.size(0), -1).to(device)
+        '''
+        desc
+        - encoder function
+            - q_phi(z|x, y) ~ "recognition" encoder network
+            - r_theta1(z|y) ~ encoder network
+        returns
+        - mu_q, logvar_q   ~ q_phi encoder output latent space representation 
+        - mu_r1, logvar_r1 ~ r_theta1 encoder output latent space representation  
+        '''
+        conv_out = self.encoder2d(self.y).to(device)                # inputs the condition through convolutional-encoder (q)
+        flattened_size = conv_out.view(conv_out.size(0), -1) 
+        flattened_x = self.x.view(self.x.size(0), -1).to(device)  
         #print(f'{flattened_size[1]}, {flattened_x[1]}')
-        xy = torch.cat((flattened_size, flattened_x), dim=1)
+        xy = torch.cat((flattened_size, flattened_x), dim=1)        # flatten layer (q)
         #print(xy)
         
-        fc_out = self.encoderLin(xy) # here we do not want the dimension, but actually the real data
-        mu_q = self.layer10(fc_out)
-        logvar_q = self.layer10(fc_out)
+        fc_out = self.encoderLin(xy)                                # flattened data through linear-encoder (q)
+        mu_q = self.layer10(fc_out)                                 # z_q mean
+        logvar_q = self.layer10(fc_out)                             # z_q log variance
 
-        conv_out_r = self.encoder2d(self.y).to(device) # apply conv layers
+        conv_out_r = self.encoder2d(self.y).to(device)              # inputs the condition through convolutional-encoder (r1)
         #print(conv_out_r.shape)
-        flattened_y_r = conv_out_r.view(conv_out_r.size(0), -1) # flatten layer
+        flattened_y_r = conv_out_r.view(conv_out_r.size(0), -1)     # flatten layer (r1)
         #print(conv_out_r.shape)
         
-        fc_out_r = self.encoderLinAlt(flattened_y_r) # apply fully conected layers
-        mu_r1 = self.layer10(fc_out_r).to(device)
-        logvar_r1 = self.layer10(fc_out_r).to(device)
+        fc_out_r = self.encoderLinAlt(flattened_y_r)                # flattened data through linear-encoder (r1)
+        mu_r1 = self.layer10(fc_out_r).to(device)                   # z_r1 mean
+        logvar_r1 = self.layer10(fc_out_r).to(device)               # z_r1 log variance
         #print(mu_r1, logvar_r1)
         
         return mu_q, logvar_q, mu_r1, logvar_r1
 
     def reparameterize(self, mu, logvar):
+        '''
+        desc
+        - reparameterize trick
+        args
+        - mu, logvar ~ mean and log variance
+        returns
+        - normal distribution with a certrain mean and standart deviation 
+        '''
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
         return mu + eps * std
 
     def decode(self, z_q):
-        conv_out = self.encoder2d(self.y).to(device)
-        flattened_y = conv_out.view(conv_out.size(0), -1)
+        '''
+        desc
+        - decoder function
+            - r_theta2(x|y, z) ~ decoder network
+        args
+        - z_q ~ latent distribution generated from the q encoder network
+        returns
+        - mu_r2, logvar_r2 ~ r_theta2 decoder output latent space representation
+        - z_r2             ~ N(mu_r2, logvar_r2²)
+        '''
+        conv_out = self.encoder2d(self.y).to(device)                # inputs the condition through convolutional-encoder (q)
+        flattened_y = conv_out.view(conv_out.size(0), -1)           # gets flattened condition to use with latent space (z_q + c)
         #print(flattened_y[1])
         #print(z_q[1])
-        zy = torch.cat((flattened_y, z_q), dim=1)
+        zy = torch.cat((flattened_y, z_q), dim=1)                   # conditioned latent space (z_q + c)
+        #print(f'\033[31;1m{zy}\033[m')
+        
+        fc_out = self.decoderLin(zy)                                # coditioned latent space through linear-decoder
+        mu_r2 = torch.sigmoid(self.layer10dec(fc_out))              # ?????????
+        logvar_r2 = -F.leaky_relu(self.layer10dec(fc_out))          # ?????????
 
-        fc_out = self.decoderLin(zy)
-        mu_r2 = torch.sigmoid(self.layer10dec(fc_out))
-        logvar_r2 = -F.leaky_relu(self.layer10dec(fc_out))
-
-        std_r2 = torch.exp(0.5 * logvar_r2)
-        eps = torch.randn_like(std_r2)
-        z_r2 = mu_r2 + eps * std_r2
-
-        return z_r2, mu_r2, logvar_r2
+        return mu_r2, logvar_r2
 
     def forward(self):
-        mu, logvar, mur1, logvarr1 = self.encode()
-        z = self.reparameterize(mu, logvar)
-        recon_data, mur2, logvarr2 = self.decode(z)
+        mu, logvar, mur1, logvarr1 = self.encode()                                  # encode
+        z = self.reparameterize(mu, logvar)                                         # latent space from encode
+        #print(f'\033[31;1m{z}\033[m') # interesting print to analize z
+        mur2, logvarr2 = self.decode(z)                                             # decode
+        recon_data = self.reparameterize(mur2, logvarr2) # z_r2                     # latent space from decode
         return recon_data, mu, logvar, mur1, logvarr1, mur2, logvarr2
 
 train_y, train_x = next(iter(train_loader))
@@ -274,11 +311,6 @@ def init_weights(m):
         nn.init.zeros_(m.bias)
 
 model.apply(init_weights)
-
-
-val_y, val_x = next(iter(val_loader))
-val_y, val_x = val_y.to(device), val_x.to(device)
-model_val = ConditionalVariationalAutoEncoder(val_x, val_y, out_features).to(device)
 
 train_loss_history, train_r2_history, val_loss_history, val_r2_history = [], [], [], []
 
@@ -480,7 +512,7 @@ for epoch in range(num_epochs):
     beta = min(0.1, epoch / 50)
     
     #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-    torch.cuda.empty_cache()
+    #torch.cuda.empty_cache()
 
     model.train()
     for y, x in train_loader:
@@ -506,16 +538,16 @@ for epoch in range(num_epochs):
     train_loss_history.append(train_epoch_loss)
     train_r2_history.append(train_epoch_r2)
     
-    model_val.eval()
+    model.eval()
     with torch.no_grad(), autocast(device_type="cuda"):  # AMP no modo de validação
-        torch.cuda.empty_cache()
+        #torch.cuda.empty_cache()
         for y_val, x_val in val_loader:
             y_val, x_val = y_val.to(device), x_val.to(device)
             x_val = x_val.view(-1, 3)
             
-            recon_data_val, mu_q_val, logvar_q_val, mu_r1_val, logvar_r1_val, mu_r2_val, logvar_r2_val = model_val()     
+            recon_data_val, mu_q_val, logvar_q_val, mu_r1_val, logvar_r1_val, mu_r2_val, logvar_r2_val = model()
             Valloss = bruh.forward(mu_q_val, logvar_q_val, mu_r1_val, logvar_r1_val, x_val, recon_data_val, beta=0.1)  
-            Valrsq = calculate_r2(recon_data, x) 
+            Valrsq = calculate_r2(recon_data_val, x_val) 
             
             val_total_loss += Valloss.detach().cpu().numpy()
             val_epoch_r2 += Valrsq * x.size(0)
@@ -568,24 +600,16 @@ plt.legend()
 plt.show()
 
 
-test_y, test_x = next(iter(test_loader))
-test_y, test_x = test_y.to(device), test_x.to(device)
-model_test = ConditionalVariationalAutoEncoder(test_x, test_y, out_features).to(device)
-model_test.eval()
+model.eval()
 predictions, real_values = [], []
 first_iteration = True
 
 with torch.no_grad():
     for y, x in test_loader:
-        recon_data, mu_q, logvar_q, mu_r1, logvar_r1, mu_r2, logvar_r2 = model_test()  # ConvVAE output
+        recon_data, mu_q, logvar_q, mu_r1, logvar_r1, mu_r2, logvar_r2 = model()
 
         predictions.append(recon_data)  # Y^ (in this case now, it's acttualy X^)
         real_values.append(x)  # Y (and here is X)
-
-        #if first_iteration:
-        #    show_images(inputs, decoded)
-        #    first_iteration = False
-
 
 # Single arrays
 predictions = torch.cat(predictions, dim=0).cpu().numpy()
